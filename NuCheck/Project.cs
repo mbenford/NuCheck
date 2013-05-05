85d7c4218e0c13b4f73b01b024cbf712dd5e9912
Michael Benford <michael@michaelbenford.net>
2013-05-05 03:19:54 -0300

    Converted properties of Project class to auto-implemented ones

--- NuCheck/Project.cs
+++ NuCheck/Project.cs
@@ -2,17 +2,13 @@
 {
     public class Project
     {
-        private readonly string name;
-        private readonly string fileName;
-
         public Project(string name, string fileName)
         {
-            this.name = name;
-            this.fileName = fileName;
+            Name = name;
+            FileName = fileName;
         }
 
-        public string Name { get { return name; } }
-
-        public string FileName { get { return fileName; } }
+        public string Name { get; private set; }
+        public string FileName { get; private set; }
     }
 }
