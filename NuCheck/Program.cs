a1b6f1f3eb14abe4754126a6f7d52a333dfc7a02
Michael Benford <michael@michaelbenford.net>
2013-05-19 18:57:45 -0300

    Implemented Program.cs first version

--- NuCheck/Program.cs
+++ NuCheck/Program.cs
@@ -8,8 +8,44 @@ namespace NuCheck
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            int exitCode = 0;
+
+            if (args.Any())
+            {
+                string solutionFile = args[0];
+                SolutionAnalyzer analyzer = CreateSolutionAnalyzer();
+                var issues = analyzer.GetIssues(solutionFile);
+
+                if (issues.Any())
+                {
+                    foreach (Issue issue in issues)
+                    {
+                        Console.WriteLine("Package {0} {1} is being referenced by the following projects:", issue.Package.Id, issue.Package.Version);
+                        foreach (Project project in issue.Projects)
+                        {
+                            Console.WriteLine(project.Name);
+                        }
+                    }
+
+                    exitCode = 1;
+                }
+                else
+                {
+                    Console.WriteLine("No issues found");
+                }
+            }
+
+            return exitCode;
+        }
+
+        private static SolutionAnalyzer CreateSolutionAnalyzer()
+        {
+            var projectExtractor = new ProjectExtractor();
+            var packagesFileLoader = new PackagesFileLoader();
+            var packagesAggregator = new PackagesAggregator(projectExtractor, packagesFileLoader);
+            return new SolutionAnalyzer(packagesAggregator);
         }
     }
 }
