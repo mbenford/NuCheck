06d47857e0e4e2159495609a42ce9fa1a956ea82
Michael Benford <michael@michaelbenford.net>
2013-05-20 01:20:34 -0300

    Refactored unit tests to use Fluent Assertions

--- NuCheck.Tests/ProjectExtractorTest.cs
+++ NuCheck.Tests/ProjectExtractorTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace NuCheck.Tests
@@ -11,10 +12,8 @@ namespace NuCheck.Tests
     {
         public class Extracts_All_Projects_From_A_Solution_File
         {
-            private IEnumerable<Project> result;
-
-            [TestFixtureSetUp]
-            public void TestFixtureSetUp()
+            [Test]
+            public void Returns_All_Projects_Found_In_A_Solution_File()
             {
                 // Arrange
                 string solutionFile = "TestData\\solution.sln";
@@ -22,55 +21,17 @@ namespace NuCheck.Tests
                 var sut = new ProjectExtractor();
 
                 // Act
-                result = sut.ExtractAll(solutionFile);
-            }
+                IEnumerable<Project> result = sut.ExtractAll(solutionFile);
 
-            [Test]
-            public void Result_Should_Not_Be_Null()
-            {
-                Assert.NotNull(result);
-            }
+                // Assert
+                var expected = new[]
+                    {
+                        new Project("Project1", "Project1.csproj"),
+                        new Project("Project2", "Project2.csproj"),
+                        new Project("Project3", "Project3.csproj"),
+                    };
 
-            [Test]
-            public void Result_Should_Contain_3_Elements()
-            {
-                Assert.AreEqual(3, result.Count());
-            }
-
-            [Test]
-            public void First_Element_Name_Should_Be_Project1()
-            {
-                Assert.AreEqual("Project1", result.ElementAt(0).Name);
-            }
-
-            [Test]
-            public void Second_Element_Name_Should_Be_Project2()
-            {
-                Assert.AreEqual("Project2", result.ElementAt(1).Name);
-            }
-
-            [Test]
-            public void Third_Element_Name_Should_Be_Project3()
-            {
-                Assert.AreEqual("Project3", result.ElementAt(2).Name);
-            }
-
-            [Test]
-            public void First_Element_FileName_Should_Be_Project1_dot_csproj()
-            {
-                Assert.AreEqual("Project1.csproj", result.ElementAt(0).FileName);
-            }
-
-            [Test]
-            public void Second_Element_FileName_Should_Be_Project2_dot_csproj()
-            {
-                Assert.AreEqual("Project2.csproj", result.ElementAt(1).FileName);
-            }
-
-            [Test]
-            public void Third_Element_FileName_Should_Be_Project2_dot_csproj()
-            {
-                Assert.AreEqual("Project3.csproj", result.ElementAt(2).FileName);
+                result.ShouldBeEquivalentTo(expected);
             }
         }
     }
