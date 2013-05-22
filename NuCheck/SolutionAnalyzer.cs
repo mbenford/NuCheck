64a4f636ba81439ea65359b1ee28637097ac22e7
Michael Benford <michael@michaelbenford.net>
2013-05-22 01:01:37 -0300

    Refactored GetIssues method

--- NuCheck/SolutionAnalyzer.cs
+++ NuCheck/SolutionAnalyzer.cs
@@ -16,18 +16,15 @@ namespace NuCheck
         {
             IDictionary<Package, IEnumerable<Project>> packagesAggregation = packagesAggregator.Aggregate(solutionFile);
 
-            var packageGroup = from package in packagesAggregation.Keys
-                               group package by package.Id
-                               into g
-                               select new { Id = g.Key, Versions = g.ToList() };
+            var packagesWithIssues = from package in packagesAggregation.Keys
+                                     group package by package.Id
+                                     into g
+                                     where g.Count() > 1
+                                     select new { Id = g.Key, Versions = g.ToList() };
 
-            var packageWithIssues = from package in packageGroup
-                                    where package.Versions.Count() > 1
-                                    select package.Versions;
-            
-            return packageWithIssues.SelectMany(package => package)
-                                    .Select(package => new Issue(package, packagesAggregation[package]))
-                                    .ToList();
+            return packagesWithIssues.SelectMany(package => package.Versions)
+                                     .Select(package => new Issue(package, packagesAggregation[package]))
+                                     .ToList();
         }
     }
 }
